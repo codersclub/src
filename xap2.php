768639dd99f51baa273e0062a547e9ec31ae5a4f
Valery Votintsev <vvotintsev@gmail.com>
2023-01-15 20:00:28 +0300

    Load backup part WZ from 2016 year

---
+++ xap2.php
@@ -0,0 +1,28 @@
+<?php
+function xap_code($login)
+{
+  $path = ''; $file = ''; $site = str_replace('www.', '',$_SERVER["HTTP_HOST"]);
+  if (strlen($_SERVER["REQUEST_URI"]) > 180) return; if ($_SERVER["REQUEST_URI"] == '') $_SERVER["REQUEST_URI"] = '/';
+  $file = base64_encode("$_SERVER[REQUEST_URI]");
+  $path_code = md5($file); $user_pref = substr($login, 0, 2);
+  $path = substr($path_code, 0, 1).'/'.substr($path_code, 1, 2).'/';
+  $domain = "$login.tnx.net";
+  $path = "/users/$user_pref/$login/$site/$path$file.txt";
+  if ($fp = fsockopen ("$domain",80, $errno, $errstr, 7))
+  {
+    fputs ($fp, "GET $path HTTP/1.0\r\nhost: $domain\r\n\r\n");
+    $fl = 0;
+    while (!feof($fp))
+    {
+      $str = trim(fgets($fp,4096));
+      if ($str == 'HTTP/1.1 404 Not Found') return;
+      if ($fl == 1) echo $str;
+      if ($str == "") $fl = 1;
+    }
+    fclose ($fp);
+  }
+}
+xap_code(strtolower("vot"));
+
+?>
+	 
---
+++ xap_network.php
@@ -0,0 +1,18 @@
+<?php 
+//$url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+//$ag = $_SERVER["HTTP_USER_AGENT"];
+//echo @file_get_contents("http://www.xap.ru/in.php?url=$url&ag=$ag"); 
+
+function xap_code ($login)
+{
+  if (getenv('HTTP_X_FORWARDED_FOR')) { $ip=getenv('HTTP_X_FORWARDED_FOR'); } else { $ip=getenv('REMOTE_ADDR');}
+  $name = $login.'##'.$ip.'##'.urldecode ($_SERVER['HTTP_HOST']).'##'.urldecode ($_SERVER['REQUEST_URI'])
+    .'##'.str_replace(strstr($_SERVER['HTTP_USER_AGENT'], '('), '',$_SERVER['HTTP_USER_AGENT']);
+  $str = ' ';
+  for ($i = 0; $i < strlen ($name) ; $i++) $str .= dechex(ord(substr($name, $i, 1)));
+    $str = trim ($str);
+    echo @file_get_contents("http://www.xap.ru/on.php?xap=$str");
+  }
+  xap_code('vot');
+
+?>
